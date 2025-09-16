cda2c3c007a97e507cf9fcc8626b006920c6ca94
MinghanWu <miw039@ucsd.edu>
2025-09-17 00:15:50 +0800

    Added dribble function; Renamed helper function

--- ai_interface/utils/basic_commands.py
+++ ai_interface/utils/basic_commands.py
@@ -1,17 +1,13 @@
 import sys
 import os
 
-from numpy._typing._array_like import NDArray
 sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 
 from typing import Iterable, List, Tuple
 import numpy as np
 from constants.player_constants import KICKABLE_MARGIN
 from constants.field_constants import GOAL_L, GOAL_R
-
-def _normalize_angle(angle: float) -> float:
-    """Normalize angle to be within [-180, 180] degrees."""
-    return (angle + 180) % 360 - 180
+from algo_utils import normalize_angle
 
 def goto(self_pose: np.ndarray | Tuple | List, x: float, y: float, margin: float = KICKABLE_MARGIN,
          theta: float | None = None, speed: float = 100.0) -> str:
@@ -20,7 +16,7 @@ def goto(self_pose: np.ndarray | Tuple | List, x: float, y: float, margin: float
     angle = np.degrees(np.arctan2(destination[1] - self_pose[1], destination[0] - self_pose[0])) - self_pose[4]
     if distance < margin:
         if theta is not None:
-            angle_diff = _normalize_angle(theta - self_pose[2])
+            angle_diff = normalize_angle(theta - self_pose[2])
             return f"turn {angle_diff}" if abs(angle_diff) > 1 else "done"
         else:
             return "done"
@@ -33,12 +29,12 @@ def shoot(self_pose: np.ndarray | Tuple | List, ball_pose: np.ndarray | Tuple | 
     if not np.isclose(self_pose[:2], ball_pose[:2], atol=kickable_tolerance):
         return "failed"
     
-    ball_dir = _normalize_angle(np.degrees(np.arctan2(ball_pose[1] - self_pose[1], ball_pose[0] - self_pose[0])))
+    ball_dir = normalize_angle(np.degrees(np.arctan2(ball_pose[1] - self_pose[1], ball_pose[0] - self_pose[0])))
     if not np.isclose(ball_dir, self_pose[2], atol=angle_tolerance):
         return "failed"
     
     angle_to_target = np.degrees(np.arctan2(target[1] - self_pose[1], target[0] - self_pose[0]))
-    angle_diff = _normalize_angle(angle_to_target - self_pose[2])
+    angle_diff = normalize_angle(angle_to_target - self_pose[2])
     return f"kick {kick_power} {angle_diff}"
 
 def shoot_at_goal(self_pose: np.ndarray | Tuple | List, ball_pose: np.ndarray | Tuple | List, 
@@ -52,9 +48,15 @@ def pass_to_teammate(self_pose: np.ndarray | Tuple | List, ball_pose: np.ndarray
     
     return shoot(self_pose, ball_pose, teammate_pose[:2], kick_power)
 
-def calculate_shooting_pose(ball_pose: np.ndarray | Tuple | List, target: np.ndarray | Tuple | List) -> np.ndarray:
+def dribble(self_pose: np.ndarray | Tuple | List, ball_pose: np.ndarray | Tuple | List,
+            kickable_tolerance: float = KICKABLE_MARGIN, angle_tolerance: float = 5.0,
+            angle: float = 0.0) -> str:
+ 
+    if not np.isclose(self_pose[:2], ball_pose[:2], atol=kickable_tolerance):
+        return "failed"
     
-    vec = np.array(ball_pose[:2]) - np.array(target)
-    destination = np.array(target) + vec + (vec / np.linalg.norm(vec)) * KICKABLE_MARGIN / 2
-    destination_theta = _normalize_angle(np.degrees(np.arctan2(vec[1], vec[0])))
-    return np.array([destination[0], destination[1], destination_theta])
+    ball_dir = normalize_angle(np.degrees(np.arctan2(ball_pose[1] - self_pose[1], ball_pose[0] - self_pose[0])))
+    if not np.isclose(ball_dir, self_pose[2], atol=angle_tolerance):
+        return "failed"
+    
+    return f"dribble {angle}"
